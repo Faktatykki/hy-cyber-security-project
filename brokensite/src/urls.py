5d7a9735102a5a36cba7ae040abf374a9b75069e
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2023-03-15 18:45:44 +0200

    removed test view

--- brokensite/src/urls.py
+++ brokensite/src/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import homePageView, somethingView, addView, registerView, secretsView
+from .views import homePageView, addView, registerView, secretsView
 
 urlpatterns = [
     path('', homePageView, name='home'),
     #path('secrets/', secretsView, name='secrets'),
     path('secrets/<str:user>/', secretsView, name='secrets'),
-    path('something/', somethingView, name='something'),
     path('add/', addView, name='add'),
     path('register/', registerView, name='register'),
 ]
