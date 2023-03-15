8e68bcd59f0588d2e270e6cbbe7b52c48eb47108
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2023-03-15 20:10:07 +0200

    removed extra line

--- brokensite/src/views.py
+++ brokensite/src/views.py
@@ -76,7 +76,6 @@ def addView(request):
     if request.method == "POST":
         input_secret = request.POST.get("secret")
 
-
         with connection.cursor() as cursor:
             statement = "INSERT INTO secrets (owner_id, content) VALUES ('" + str(logged_user) + "', '" + str(input_secret) +"')"
             cursor.executescript(statement)
